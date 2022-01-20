b96b96950b8035a4d871c3fb36f49b776704e857
Laith Qasem <laithqasem888>
2022-01-20 04:06:43 +0200

    Done

--- src/main/java/com/database/database/HelloApplication.java
+++ src/main/java/com/database/database/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-       FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainMenu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 620);
+       FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("loginMenu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("<<<Oregano>>>");
         stage.setScene(scene);
         stage.show();
